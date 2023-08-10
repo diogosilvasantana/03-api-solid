03dddb6a3739e25c0d9e7b00003737a88f3163c6
Proart Studio <proartstudio@iMac-Pro-de-Proart.local>
2023-08-09 22:04:44 -0300

    Update

--- src/usecases/check-in.usecase.spec.ts
+++ src/usecases/check-in.usecase.spec.ts
@@ -1,24 +1,67 @@
-import { expect, describe, it, beforeEach } from 'vitest'
-import { hash } from 'bcryptjs'
-
+import { expect, describe, it, beforeEach, vi, afterEach } from 'vitest'
 import { CheckingUseCase } from './check-in.usecase'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { Decimal } from '@prisma/client/runtime/library'
 
 let checkinsRepository: InMemoryCheckInsRepository
+let gymsRepository: InMemoryGymsRepository
 let checkingUseCase: CheckingUseCase
 
 describe('Get User Profile Use Case', () => {
   beforeEach(() => {
     checkinsRepository = new InMemoryCheckInsRepository()
-    checkingUseCase = new CheckingUseCase(checkinsRepository)
+    gymsRepository = new InMemoryGymsRepository()
+    checkingUseCase = new CheckingUseCase(checkinsRepository, gymsRepository)
+
+    gymsRepository.items.push({
+      id: 'gym-01',
+      title: 'Academia 1',
+      description: 'Teste',
+      phone: '',
+      latitude: new Decimal(0),
+      longitude: new Decimal(0),
+    })
+
+    vi.useFakeTimers()
   })
 
-  it('should be able to get user profile', async () => {
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
+  it('should be able to check in', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
     const { checkIn } = await checkingUseCase.execute({
       userId: 'user-01',
       gymId: 'gym-01',
+      userLatitude: 0,
+      userLongitude: 0,
     })
 
+    console.log(checkIn.created_at)
+
     expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to check in twice in the same day', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
+    await checkingUseCase.execute({
+      userId: 'user-01',
+      gymId: 'gym-01',
+      userLatitude: 0,
+      userLongitude: 0,
+    })
+
+    await expect(() =>
+      checkingUseCase.execute({
+        userId: 'user-01',
+        gymId: 'gym-01',
+        userLatitude: 0,
+        userLongitude: 0,
+      }),
+    ).rejects.toBeInstanceOf(Error)
   })
 })
